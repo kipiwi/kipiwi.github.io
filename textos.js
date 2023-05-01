5fbf1b10ea234074a7c2a50cc5323e9b230d3721
kipiwi <132236202+kipiwi@users.noreply.github.com>
2023-04-30 21:47:00 -0300

    Add files via upload

---
+++ textos.js
@@ -0,0 +1,18 @@
+var textos = [
+    '<p style="font-size:2vw;">"Kipiwi: Onde os downloads são tão fáceis que até sua avó consegue!"</p>',
+    '<p style="font-size:2vw;">"Kipiwi: Aqui você pode baixar mais coisas do que na bolsa de valores!"</p>',
+    '<p style="font-size:2vw;">"Kipiwi: O paraíso dos downloads grátis, porque pagar é coisa de gente rica!"</p>',
+    '<p style="font-size:2vw;">"Kipiwi: Baixar sem limites, porque a vida já tem problemas demais!"</p>',
+    '<p style="font-size:2vw;">"Kipiwi: O melhor lugar para fazer aquele downloadzinho esperto!"</p>',
+    '<p style="font-size:2vw;">"Kipiwi: Onde o download é grátis e a felicidade é ilimitada!"</p>',
+    '<p style="font-size:2vw;">"Kipiwi: Baixe agora e pague com um sorriso!"</p>',
+    '<p style="font-size:2vw;">"Kipiwi: O lugar onde os downloads são grátis e a diversão é garantida!"</p>',
+    '<p style="font-size:2vw;">"Kipiwi: O seu destino para downloads gratuitos e risadas infinitas!"</p>',
+    '<p style="font-size:2vw;">"Kipiwi: Onde você pode baixar tudo o que precisa e ainda economizar para o happy hour!"</p>',
+    '<p style="font-size:2vw;">"Kipiwi: O lugar onde os downloads são tão rápidos que você nem tem tempo de sentir saudade do botão de download!"</p>',
+    '<p style="font-size:2vw;">"Kipiwi: O paraíso dos downloads grátis, onde a única coisa que você precisa pagar é com a sua gratidão!"</p>'
+  ];
+  
+  var index = Math.floor(Math.random() * textos.length);
+  
+  document.write(textos[index]);
